c03964135f947022873c0d29798f0bb7d471e336
Natividad Alvarez <tivialvarez@gmail.com>
2020-09-08 01:16:24 -0400

    Created validatePost middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./userDb');
+const { restart } = require('nodemon');
 
 const router = express.Router();
 
@@ -7,7 +8,7 @@ router.post('/', (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validatePost, (req, res) => {
   // do your magic!
 });
 
@@ -20,8 +21,8 @@ router.get('/', (req, res, next) => {
   });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -40,6 +41,13 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  db.getById(req.params.id).then(user=>{
+    if(!user) return res.status(400).json({message: "invalid user id"});
+    req.user = user;
+    next();
+  }).catch(err=>{
+    next(err);
+  });
 }
 
 function validateUser(req, res, next) {
@@ -48,6 +56,9 @@ function validateUser(req, res, next) {
 
 function validatePost(req, res, next) {
   // do your magic!
+  if(!req.body) return res.status(400).json({message: "missing post data"});
+  if(!req.body.text) return res.status(400).json({message: "missing required text field"});
+  next();
 }
 
 module.exports = router;
